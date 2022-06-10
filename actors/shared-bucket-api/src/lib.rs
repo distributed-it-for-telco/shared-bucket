9c75be9b68e593a810c938f529113b0c47054b95
Yann Cardon <yann.cardon@orange.com>
2022-06-10 16:58:35 +0200

    fix: log to info

--- actors/shared-bucket-api/src/lib.rs
+++ actors/shared-bucket-api/src/lib.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use shared_bucket::{Customer, Customers, CustomersSender};
 use wasmbus_rpc::actor::prelude::*;
 use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
-use wasmcloud_interface_logging::debug;
+use wasmcloud_interface_logging::info;
 
 const CUSTOMERS_ACTOR: &str = "customers"; //TODO not sure about the prefix here
 const SERVICE_VENDORS_ACTOR: &str = "service_vendors"; //TODO not sure about anything here
@@ -24,11 +24,11 @@ impl HttpServer for SharedBucketAPIActor {
         ctx: &Context,
         req: &HttpRequest,
     ) -> std::result::Result<HttpResponse, RpcError> {
-        debug!("API request: {:?}", req);
+        info!("API request: {:?}", req);
 
         let path = &req.path[1..req.path.len()];
         let segments: Vec<&str> = path.trim_end_matches('/').split('/').collect();
-        debug!("Segments: {:?}", segments);
+        info!("Segments: {:?}", segments);
 
         match (req.method.as_ref(), segments.as_slice()) {
             ("POST", ["customers"]) => create_customer(ctx, deser(&req.body)?).await,
