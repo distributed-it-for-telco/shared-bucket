f97cb713b8122d9e628e922d838dcd8339214353
MonsieurHulot <arnaud.boulay@orange.com>
2022-06-10 14:30:41 +0200

    Dropping HttpService for ServiceVendors Actor (A2A only)

--- actors/service-vendors/src/lib.rs
+++ actors/service-vendors/src/lib.rs
@@ -1,33 +1,11 @@
 use wasmbus_rpc::actor::prelude::*;
-use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
 use shared_bucket::{Customers};
 use wasmcloud_interface_logging::{info};
 
 #[derive(Debug, Default, Actor, HealthResponder)]
-#[services(Actor, HttpServer)]
+#[services(Actor)]
 struct ServiceVendorsActor {}
 
-/// Implementation of HttpServer trait methods
-#[async_trait]
-impl HttpServer for ServiceVendorsActor {
+// Implementation of trait methods : TODO
 
-    /// Returns a greeting, "Hello World", in the response body.
-    /// If the request contains a query parameter 'name=NAME', the
-    /// response is changed to "Hello NAME"
-    async fn handle_request(
-        &self,
-        _ctx: &Context,
-        req: &HttpRequest,
-    ) -> std::result::Result<HttpResponse, RpcError> {
-        let text = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "name")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "World".to_string());
-
-        Ok(HttpResponse {
-            body: format!("Hello {}", text).as_bytes().to_vec(),
-            ..Default::default()
-        })
-    }
-}
 
