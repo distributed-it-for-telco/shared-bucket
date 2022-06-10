5a1ed8ac92d8ae7c45fa9575de6574859ce3e067
Yann Cardon <yann.cardon@orange.com>
2022-06-10 11:47:12 +0200

    wip: implement REST api

--- actors/shared-bucket-api/src/lib.rs
+++ actors/shared-bucket-api/src/lib.rs
@@ -1,5 +1,11 @@
 use wasmbus_rpc::actor::prelude::*;
 use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use shared_bucket::{Customers};
+use wasmcloud_interface_logging::debug;
+
+const CUSTOMERS_ACTORS: &str = "shared_bucket/customers"; //TODO not sure about the prefix here
+const VISITS_ACTOR: &str = "shared_bucket/visits";
+
 
 #[derive(Debug, Default, Actor, HealthResponder)]
 #[services(Actor, HttpServer)]
@@ -9,23 +15,20 @@ struct SharedBucketAPIActor {}
 #[async_trait]
 impl HttpServer for SharedBucketAPIActor {
 
-    /// Returns a greeting, "Hello World", in the response body.
-    /// If the request contains a query parameter 'name=NAME', the
-    /// response is changed to "Hello NAME"
     async fn handle_request(
         &self,
         _ctx: &Context,
         req: &HttpRequest,
     ) -> std::result::Result<HttpResponse, RpcError> {
-        let text = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "name")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "World".to_string());
+        debug!("API request: {:?}", req);
 
-        Ok(HttpResponse {
-            body: format!("Hello {}", text).as_bytes().to_vec(),
-            ..Default::default()
-        })
+        let path = &req.path[1..req.path.len()];
+        let segments: Vec<&str> = path.trim_end_matches('/').split('/').collect();
+        debug!("Segments: {:?}", segments);
+
+        match (req.method.as_ref(), segments.as_slice()) {
+            ("GET", ["customers", customer_id]) => todo!(),
+            (_, _) => Ok(HttpResponse::not_found()),
+       }
     }
 }
-
